6d0675aaf101d73e31cd5ac6547d52908a656f78
Sheena Bonilla <sheena.myb@gmail.com>
2020-05-16 17:04:30 +0000

    Automatically backed up by Learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelpers
-end
-def title(text)
-  content_for :title, text #sends text to the place in the app layout that is expecting some contetnt for the :title
+  def title(text)
+    content_for :title, text #sends text to the place in the app layout that is expecting some contetnt for the :title
+  end
+
 end
